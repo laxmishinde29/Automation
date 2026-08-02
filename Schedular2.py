b8b3bd92ead32d1b9e089a211395323e5748e714
laxmishinde29 <laxmishinde2975@gmail.com>
2026-08-02 19:00:03 +0530

    Add files via upload

---
+++ Schedular2.py
@@ -0,0 +1,16 @@
+import time
+import datetime
+import schedule
+
+def fun():
+    print("Inside fun at : ",datetime.datetime.now())
+
+def main():
+    print("Inside Marvellous Automation script at :",datetime.datetime.now())
+
+    schedule.every(20).seconds.do(fun)      #replace with hours or miniute
+
+    #code has problem
+
+if __name__ =="__main__":
+    main()
---
+++ Schedular4.py
@@ -0,0 +1,24 @@
+import time
+import datetime
+import schedule
+
+def fun():
+    print("Inside fun at : ",datetime.datetime.now())
+
+def gun():
+    print("Inside gun at : ",datetime.datetime.now())
+
+def main():
+    print("Inside Marvellous Automation script at :",datetime.datetime.now())
+
+    #schedule.every(20).seconds.do(fun)      #replace with hours or minute
+    schedule.every(1).minute.do(fun)
+
+    schedule.every(1).hours.do(gun)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ =="__main__":
+    main()
