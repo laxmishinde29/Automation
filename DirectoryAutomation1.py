7cd1658737be16d0dc2887dab707b0464d8da8df
laxmishinde29 <laxmishinde2975@gmail.com>
2026-08-05 19:29:18 +0530

    Add files via upload

---
+++ DirectoryAutomation1.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+def DirectoryScanner(DirName = "Marvellous"):
+    Ret = False
+
+    Ret = os.path.exists(DirName)
+    if(Ret == False):
+        print("There is no such directory")
+        return
+
+    Ret = os.path.isdir(DirName)
+    if(Ret == False):
+        print("It is not a directory")
+        return
+
+    for FolderName, SubFolderName, FileName in os.walk(DirName):
+        for fname in FileName:
+            print(fname)
+
+def main():
+    Border = "-"*50
+
+    print(Border)
+    print("--------- Marvellous Directory Automation ---------")
+    print(Border)
+
+    if(len(sys.argv) != 2):
+        print("Invalid number of arguments")
+        print("Please specify the name of directory")
+        return
+
+    DirectoryScanner(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
---
+++ DirectoryAutomation2.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+def DirectoryScanner(DirName = "Marvellous"):
+    Ret = False
+
+    Ret = os.path.exists(DirName)
+    if(Ret == False):
+        print("There is no such directory")
+        return
+
+    Ret = os.path.isdir(DirName)
+    if(Ret == False):
+        print("It is not a directory")
+        return
+
+    for FolderName, SubFolderName, FileName in os.walk(DirName):
+        for fname in FileName:
+            print("File Name: ",fname)
+            print("File Size: ",os.path.getsize(fname))             #pathissues
+
+def main():
+    Border = "-"*50
+
+    print(Border)
+    print("--------- Marvellous Directory Automation ---------")
+    print(Border)
+
+    if(len(sys.argv) != 2):
+        print("Invalid number of arguments")
+        print("Please specify the name of directory")
+        return
+
+    DirectoryScanner(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
