36ea3208f05aac43ae1b431f3a18a1bd843e76c2
laxmishinde29 <laxmishinde2975@gmail.com>
2026-08-10 19:17:38 +0530

    Add files via upload

---
+++ DirectoryAutomationEmptyDelete.py
@@ -0,0 +1,41 @@
+import sys
+import os
+
+def DirectoryScanner(DirName = "Marvellous"):
+    Ret = False
+
+    Ret = os.path.exists(DirName)
+    if(Ret == False):
+        print("There is no such directory")
+        return
+
+    Ret = os.path.isdir(DirName)
+    if(Ret == False):
+        print("It is not a directory")
+        return
+
+    for FolderName, SubFolderName, FileName in os.walk(DirName):
+        for fname in FileName:
+            fname = os.path.join(FolderName,fname)
+            print("File Name: ",fname)
+            print("File Size: ",os.path.getsize(fname),"bytes")    
+
+            if(os.path.getsize(fname) == 0):             #empty files ahe ka
+                os.remove(fname)
+
+def main():
+    Border = "-"*50
+
+    print(Border)
+    print("--------- Marvellous Directory Automation ---------")
+    print(Border)
+
+    if(len(sys.argv) != 2):
+        print("Invalid number of arguments")
+        print("Please specify the name of directory")
+        return
+
+    DirectoryScanner(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
