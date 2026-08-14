903348b31dece36e7458aae7488b0019c5da4727
laxmishinde29 <laxmishinde2975@gmail.com>
2026-08-14 18:04:24 +0530

    Add files via upload

---
+++ CheckSumCalculate.py
@@ -0,0 +1,24 @@
+import hashlib                  #os dependent module
+
+def CalculateChecksum(FileName):
+    fobj = open(FileName, "rb")
+
+    hobj = hashlib.md5()
+
+    Buffer = fobj.read(1000)
+
+    while(len(Buffer) > 0):
+        hobj.update(Buffer)
+        Buffer = fobj.read(1000)
+
+    fobj.close()
+
+    return hobj.hexdigest()
+
+def main():
+    Ret = CalculateChecksum("Demo.txt")
+
+    print("CheckSum is :",Ret)
+
+if __name__ == "__main__":
+    main()
